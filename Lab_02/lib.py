94f16e4081f3a3005097ccf3f242a2b16e80263e
ancestor-mithril <sgeorge.sstoica99@gmail.com>
2021-03-02 13:24:02 +0200

    Added read from file, stdin and random matrices
    also tried bonus

--- Lab_02/lib.py
+++ Lab_02/lib.py
@@ -1,5 +1,8 @@
+import itertools
+
 import numpy as np
 import functions as ff
+
 
 def cholesky_decomposition(a):
     n = len(a)
@@ -60,3 +63,37 @@ def system_solve(a, b):
             value -= l[j][i] * x[j]
         x[j] = value / l[i][i]
     return x
+
+
+def bonus(a, b):
+    print(a)
+    a_vec = [a[x][i] for x in range(len(a)) for i in range(x + 1)]
+    print(a_vec)
+
+    def bonus_cholesky_decomposition(a):
+        n = int((len(a) * 2) ** 0.5)
+        l_vec = [0 for i in range(n * (n + 1) // 2)]
+        before = 0
+        for i in range(n):
+            for j in range(i + 1):
+                value = a[i + before + j]
+                if j == i:
+                    for k in range(j):
+                        value -= l_vec[j + before + k] ** 2
+                    if value < 0:
+                        raise Exception("Matrix is not positive 2")
+                    l_vec[i + before + j] = np.sqrt(value)  # int(np.sqrt(value))
+                else:
+                    for k in range(j):
+                        value -= l_vec[i + before + k] * l_vec[j + before + k]
+                    if value == 0:
+                        raise Exception("Matrix is not positive 1")
+                    value /= l_vec[j + before + j]
+                    l_vec[i + before + j] = value  # int(value)
+            before = i + before
+        return l
+
+    print(a)
+    print("ok")
+    l = bonus_cholesky_decomposition(a_vec)
+    print(l)
---
+++ Lab_02/test.py
@@ -0,0 +1,120 @@
+import unittest
+import numpy as np
+import scipy
+import scipy.linalg
+import random
+import lib
+import functions as ff
+
+
+class CholeskyDecomposition(unittest.TestCase):
+    m1 = [[4, 2, 8], [2, 10, 10], [8, 10, 21]]
+    A = [
+        [2.25, 3, 3],
+        [3, 9.0625, 13],
+        [3, 13, 24]
+    ]
+    b = [9, 35.0625, 61]
+    x = [0, 1, 2]
+
+    def test_1_decomp(self):
+        read_from_file()
+        # read_from_stdin()
+        # random_init(3)
+        cd = CholeskyDecomposition
+
+        A = np.array(cd.m1)
+        L = scipy.linalg.cholesky(A, lower=True)
+        U = scipy.linalg.cholesky(A, lower=False)
+
+        res = lib.cholesky_decomposition(cd.m1)
+        np.testing.assert_array_almost_equal(res, L)
+
+    def test_2_determA(self):
+        cd = CholeskyDecomposition
+
+        det = lib.determ_A(cd.m1)
+
+        np.testing.assert_array_almost_equal(np.linalg.det(cd.m1), det)
+
+    def test_3_X_chol(self):
+        cd = CholeskyDecomposition
+
+        let = lib.system_solve(cd.A, cd.b)
+        let2 = np.linalg.solve(cd.A, cd.b)
+        np.testing.assert_array_almost_equal(let, let2)
+
+    def test_4_tsolve(self):
+        cd = CholeskyDecomposition
+
+        x_chol = lib.system_solve(cd.A, cd.b)
+        a_x_chol = ff.get_matrix_mul(cd.A, x_chol)
+        err = 0
+
+        for i in range(len(a_x_chol)):
+            err += (a_x_chol[i] - cd.b[i]) ** 2
+        err = err ** 0.5
+
+        aux = np.subtract(a_x_chol, cd.b)
+        self.assertAlmostEqual(np.linalg.norm(aux, 2), err)
+
+    def test_5_lu(self):
+        cd = CholeskyDecomposition
+
+        p, l, u = scipy.linalg.lu(cd.A)
+        print(p, l, u)
+        x = np.linalg.solve(cd.A, cd.b)
+        print(x)
+        self.assertTrue(True)
+
+    def test_6_inverse(self):
+        cd = CholeskyDecomposition
+
+        if ff.get_matrix_determinant(cd.A) == 0:
+            raise Exception("Invalid matrix")
+        inv = ff.get_matrix_inverse(cd.A)
+        inv2 = np.linalg.inv(cd.A)
+        np.testing.assert_array_almost_equal(inv, inv2)
+
+        print("norm", np.linalg.norm(np.subtract(inv, inv2)))
+
+
+def read_from_file():
+    CholeskyDecomposition.A = eval(open("A.txt", "r").read())
+    CholeskyDecomposition.m1 = CholeskyDecomposition.A
+    CholeskyDecomposition.b = eval(open("b.txt", "r").read())
+
+
+def read_from_stdin():
+    print(
+        "Se presupune ca utilizatorul introduce date corecte, dimensiunea matricii un int, si fiecare element un float"
+    )
+    n = int(input("Introdu dimensiunea matricii A:"))
+    A = []
+    for i in range(n):
+        line = []
+        for j in range(n):
+            x = float(input(f"Introdu elementul de pe linia {i}, coloana {j}"))
+            line.append(x)
+        A.append(line)
+    print(A)
+    print("Citeste valorile din b")
+    b = []
+    for i in range(n):
+        x = float(input(f"Introdu elementul {i} din b"))
+        b.append(x)
+    CholeskyDecomposition.A = A
+    CholeskyDecomposition.m1 = CholeskyDecomposition.A
+    CholeskyDecomposition.b = b
+
+
+def random_init(n):
+    CholeskyDecomposition.A = [[random.randint(-10, 10) for _ in range(n)] for _ in range(n)]
+    CholeskyDecomposition.m1 = CholeskyDecomposition.A
+    CholeskyDecomposition.b = [random.randint(-10, 10) for _ in range(n)]
+
+
+if __name__ == "__main__":
+
+    unittest.main()
+
